dc9c827eb036f0340bb29eb073e0b40806a7d19b
Noah Cordova <noahco86@gmail.com>
2022-03-01 17:17:33 -0800

    added Model folder and json obj

---
+++ Pokedex/Pokedex/Model/PokemonListItem.swift
@@ -0,0 +1,15 @@
+//
+//  PokemonListItem.swift
+//  Pokedex
+//
+//  Created by Noah Cordova on 3/1/22.
+//
+
+import Foundation
+
+
+struct PokemonListItem: Hashable, Codable {
+    var name: String
+    var url: String
+    
+}
