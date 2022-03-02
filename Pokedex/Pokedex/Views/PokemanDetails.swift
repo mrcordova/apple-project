4d35e60eb663f2907b846fd149edcb5832b8f39b
Noah Cordova <noahco86@gmail.com>
2022-03-01 20:02:23 -0800

    created pokemondeatials view

---
+++ Pokedex/Pokedex/Views/PokemanDetails.swift
@@ -0,0 +1,20 @@
+//
+//  PokemanDetails.swift
+//  Pokedex
+//
+//  Created by Noah Cordova on 3/1/22.
+//
+
+import SwiftUI
+
+struct PokemanDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemanDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemanDetails()
+    }
+}
