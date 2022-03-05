d142ccab9453efba7fa04d3cdd23f6fc6dd2e5b2
Noah Cordova <noahco86@gmail.com>
2022-03-05 14:56:22 -0800

    created intro view

---
+++ Pokedex/Pokedex/Views/Intro.swift
@@ -0,0 +1,24 @@
+//
+//  Intro.swift
+//  Pokedex
+//
+//  Created by Noah Cordova on 3/5/22.
+//
+
+import SwiftUI
+
+struct Intro: View {
+    var body: some View {
+        Button(action: {
+                       
+                 }, label: {Text("Btn")}).padding()
+       
+        
+    }
+}
+
+struct Intro_Previews: PreviewProvider {
+    static var previews: some View {
+        Intro()
+    }
+}
